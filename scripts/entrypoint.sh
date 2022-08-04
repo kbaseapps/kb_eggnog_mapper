f28a32ec56a605f0a483a9cc679cb83d11dddf9f
dcchivian <dcchivian@lbl.gov>
2022-08-04 21:00:41 +0000

    hide refdata for now to debug module reg

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -18,22 +18,22 @@ elif [ "${1}" = "async" ] ; then
 elif [ "${1}" = "init" ] ; then
   echo "Initialize module"
 
-  EGGNOG_DB_DOWNLOAD=/kb/module/eggnog-mapper/download_eggnog_data.py
-  
-  EGGNOG_DB_VER=5.0.2
-  EGGNOG_DB_DIR=/data/eggnogdb/$EGGNOG_DB_VER
-  mkdir -p $EGGNOG_DB_DIR
-  cd $EGGNOG_DB_DIR
-
-  echo "running $EGGNOG_DB_DOWNLOAD -y --data_dir=$EGGNOG_DB_DIR"
-  $EGGNOG_DB_DOWNLOAD -y --data_dir=$EGGNOG_DB_DIR
-
-  if [ -s "$EGGNOG_DB_DIR/eggnog.db" -a -s "$EGGNOG_DB_DIR/eggnog.taxa.db" -a -s "$EGGNOG_DB_DIR/eggnog.taxa.db.traverse.pkl" -a -s "$EGGNOG_DB_DIR/eggnog_proteins.dmnd" -a -s "$EGGNOG_DB_DIR/novel_fams.dmnd" -a -s "$EGGNOG_DB_DIR/mmseqs/mmseqs.db" -a -s "$EGGNOG_DB_DIR/Pfam-A.hmm" ] ; then
-    echo "DATA DOWNLOADED SUCCESSFULLY"
-    touch /data/__READY__
-  else
-    echo "Init failed"
-  fi  
+#  EGGNOG_DB_DOWNLOAD=/kb/module/eggnog-mapper/download_eggnog_data.py
+#  
+#  EGGNOG_DB_VER=5.0.2
+#  EGGNOG_DB_DIR=/data/eggnogdb/$EGGNOG_DB_VER
+#  mkdir -p $EGGNOG_DB_DIR
+#  cd $EGGNOG_DB_DIR
+#
+#  echo "running $EGGNOG_DB_DOWNLOAD -y --data_dir=$EGGNOG_DB_DIR"
+#  $EGGNOG_DB_DOWNLOAD -y --data_dir=$EGGNOG_DB_DIR
+#
+#  if [ -s "$EGGNOG_DB_DIR/eggnog.db" -a -s "$EGGNOG_DB_DIR/eggnog.taxa.db" -a -s "$EGGNOG_DB_DIR/eggnog.taxa.db.traverse.pkl" -a -s "$EGGNOG_DB_DIR/eggnog_proteins.dmnd" -a -s "$EGGNOG_DB_DIR/novel_fams.dmnd" -a -s "$EGGNOG_DB_DIR/mmseqs/mmseqs.db" -a -s "$EGGNOG_DB_DIR/Pfam-A.hmm" ] ; then
+#    echo "DATA DOWNLOADED SUCCESSFULLY"
+#    touch /data/__READY__
+#  else
+#    echo "Init failed"
+#  fi  
 elif [ "${1}" = "bash" ] ; then
   bash
 elif [ "${1}" = "report" ] ; then
